2feab9f083ebdb11dba6470e7936dc16f211c3c4
noapeleg <noapeleg2002@gmail.com>
2025-11-13 08:33:42 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,14 +1,14 @@
 def find_max_number(num1, num2, num3):
-if num1 > num2:
-  if num1 > num3:
-    return num1
-  else:
-    return num3
-  elif num2 > num3:
-      return num2
-  else:
-      return num3
-        
+    if num1 > num2:
+        if num1 > num3:
+            return num1
+        else:
+            return num3
+    elif num2 > num3:
+        return num2
+    else:
+        return num3
+      
 def find_mean(num1, num2, num3):
   mean = (num1 + num2 + num3) / 3 
   return mean
